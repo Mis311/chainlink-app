32bae2085b62ec0cb901b2e5c428ecd3c8adda1d
Mis311 <ley.hanamura@gmail.com>
2023-06-09 02:15:18 +0200

    change

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import "react-quill/dist/quill.snow.css";
-import "../src/pages/globals.css";
+import "./globals.css";
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
