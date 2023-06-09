b40d6f3b39d360eb40e260fe6c40ea125becec1d
Mis311 <ley.hanamura@gmail.com>
2023-06-09 02:07:07 +0200

    linking dashboard

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,14 +21,18 @@ export default function Home() {
         <p className="text-xl mb-8">
           Empowering writers with AI assistance and collaboration tools.
         </p>
-        <button className="btn btn-primary">Start Writing Now</button>
+        <Link href="./dashboard">
+          <button className="btn btn-primary">Start Writing Now</button>
+        </Link>
       </section>
 
       {/* Company Section 1 */}
       <section className="p-10">
         <h2 className="text-3xl font-bold mb-4">Our Story</h2>
         <p className="text-xl mb-8">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam.
         </p>
       </section>
 
@@ -36,7 +40,9 @@ export default function Home() {
       <section className="p-10">
         <h2 className="text-3xl font-bold mb-4">Our Mission</h2>
         <p className="text-xl mb-8">
-          Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+          velit esse cillum dolore eu fugiat nulla pariatur.
         </p>
       </section>
 
@@ -44,7 +50,10 @@ export default function Home() {
       <section className="p-10">
         <h2 className="text-3xl font-bold mb-4">Our Team</h2>
         <p className="text-xl mb-8">
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+          officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde
+          omnis iste natus error sit voluptatem accusantium doloremque
+          laudantium.
         </p>
       </section>
     </div>
